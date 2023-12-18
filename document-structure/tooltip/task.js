1398ad72deffc476d0f963d047e6ce5296248200
gukasyan <mishagukasyan@mail.ru>
2023-12-18 14:07:07 +0700

    change task

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,12 +2,14 @@
 
 const toolTipList = Array.from(document.querySelectorAll('a.has-tooltip'));
 let innerTooltipText;
+const { top, bottom, left } = tip.getBoundingClientRect();
 
 toolTipList.forEach(function (tip) {
     tip.onclick = function (event) {
+        event.preventDefault();
         let tooltip = document.querySelector('div.tooltip_active');
-        let topCoordinate = tip.getBoundingClientRect().top < window.innerHeight / 2 ?  tip.getBoundingClientRect().bottom + 10 : tip.getBoundingClientRect().top - 30;
-        let leftCoordinate = tip.getBoundingClientRect().left < window.innerWidth / 2 ? tip.getBoundingClientRect().left + 10 : tip.getBoundingClientRect().left - 10;
+        let topCoordinate = top < window.innerHeight / 2 ?  bottom + 10 : top - 30;
+        let leftCoordinate = left < window.innerWidth / 2 ? left + 10 : left - 10;
         let html = `<div class="tooltip tooltip_active" style="left: ${leftCoordinate}px; top: ${topCoordinate}px">${tip.title}</div>`;
         if (tooltip && event.path[0].innerText == innerTooltipText) {
             tooltip.classList.toggle('tooltip_active');
